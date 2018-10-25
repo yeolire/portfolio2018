d705c83272388c5d7bac382b434f4026ef5e2853
GAEGOOL <yeolire@iclould.com>
2018-10-25 10:46:45 +0900

    bodabom/edit subpages HTML

--- bodabom/js/common.js
+++ bodabom/js/common.js
@@ -1,6 +1,5 @@
 toggleMenu();
 hideNav();
-// tabMenu();
 
 function toggleMenu(){
   const nav=document.querySelector('header nav');
@@ -15,10 +14,10 @@ function toggleMenu(){
     this.classList.remove('on');
     btn[0].className==='on' ? nav.style.display='none': nav.style.display='block';
   }
-  var mql = window.matchMedia("screen and (min-width: 700px)")
+  var mql = window.matchMedia("screen and (min-width: 700px)");
   mql.addListener(function(e) {
       if(e.matches) {
-        nav.style.display='flex';
+        nav.style.display='flex'||'block';
       }else{
         nav.style.display='none';
         btn[0].classList.add('on');
@@ -43,22 +42,5 @@ function hideNav(){
       nav.style.top='-80px';
     }
     prev=current;
-  }
-}
-
-function tabMenu(){
-  const btns=Array.from(document.querySelectorAll(''));
-  btns.forEach(function(btn){
-    btn.addEventListener('click',open);
-  });
-  function open(){
-    const contents=document.querySelectorAll('');
-    for(let i=0, item;item=btns[i];i++){
-      btns[i].classList.remove('active');
-      contents[i].classList.remove('active');
-    }
-    this.classList.add('active');
-    let i=btns.indexOf(this);
-    contents[i].classList.add('active');
   }
 }
--- bodabom/js/home.js
+++ bodabom/js/home.js
@@ -8,6 +8,6 @@ function changeBg(){
       count=1;
     }
     const bg=document.querySelector('.bg');
-    bg.style.backgroundImage=`url(./img/home0${count}.jpg)`;
+    bg.style.backgroundImage=`url(img/home0${count}.jpg)`;
   }, 3000)
 }
