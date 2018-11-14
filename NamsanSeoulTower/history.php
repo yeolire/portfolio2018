9c6bb5f28bc798b4f6cbee94199450b8805153f8
GAEGOOL <yeolire@gmail.com>
2018-11-14 18:18:44 +0900

    add

---
+++ NamsanSeoulTower/history.php
@@ -0,0 +1,14 @@
+<? include_once ('header.php'); ?>
+
+<!-- contents -->
+<main id="sub-history" class="container-fluid p-0">
+  <hgroup id="sub-title">
+    <h2>남산서울타워의 역사</h2>
+    <p>남산서울타워가 쌓아온 40여년의 기록</p>
+  </hgroup>
+  <section class="container">
+    <img src="img/img_tower_history.png">
+  </section>
+</main>
+
+<? include_once ('footer.php'); ?>
