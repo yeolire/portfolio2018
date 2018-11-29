73a60bfb187def4484c91fd3184ff38130ad22e6
EunJin <yeolire@gmail.com>
2018-11-29 19:02:30 +0900

    oo--edit

--- OO/js/common.js
+++ OO/js/common.js
@@ -1,20 +1,20 @@
 const toggle = (function() {
   const btns = Array.from(document.querySelectorAll('.toggle'));
 
+  const show=function() {
+    const remove = document.querySelector('.remove');
+    const lists = document.querySelector('.nav__links');
+
+    for (let i = 0, item; item = btns[i]; i++) {
+      btns[i].classList.toggle('on');
+    }
+    lists.classList.toggle('on');
+    remove.classList.toggle('on');
+  }
+
   btns.forEach(function(btn){
-    btn.addEventListener('click', function() {
-      const links = document.querySelector('.nav__links');
-      const remove = document.querySelector('.remove');
-
-      for (let i = 0, item; item = btns[i]; i++) {
-        btns[i].classList.toggle('on');
-      }
-      links.classList.toggle('on');
-      remove.classList.toggle('on');
-    });
+    btn.addEventListener('click', show);
   });
-
-
 })();
 
 
