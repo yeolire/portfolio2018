7c7c4017c97f4f0e76c0e1e418ce3bd8643d0369
EunJin <yeolire@gmail.com>
2018-12-01 16:48:47 +0900

    pig--fix error next -> reset prev dot

--- pigGame/app.js
+++ pigGame/app.js
@@ -76,6 +76,8 @@ function nextPlayer() {
   dice.classList.add
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   roundScore = 0;
+  prevDot = null;
+  secondPrevDot = null;
 
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
