b410a84e6f56e7ca8de0163e9bfe402d8c8471ee
EunJin <yeolire@gmail.com>
2018-12-04 09:51:05 +0900

    seoul--js

--- NamsanSeoulTower/main.js
+++ NamsanSeoulTower/main.js
@@ -4,29 +4,27 @@ var url = require('url');
 var qs = require('querystring');
 var path = require('path');
 
-var template = require('lib/template.js');
+var template = require('./lib/template.js');
 
 var app = http.createServer(function(request, response) {
-var _url = request.url;
-var queryData = url.parse(_url, true).query;
-var pathname = url.parse(_url, true).pathname;
-if (pathname === '/') {
-  fs.readdir('./data', function(err, filelist) {
-    var filteredId = path.parse(queryData.id).base;
-    if (queryData.id === undefined) {
-      filteredId = 'index';
-    }
-    fs.readFile(`./data/${filteredId}`, 'utf-8', function(err, contents) {
-      var html = template.html(contents);
-      response.writeHead(200);
-      response.end(html);
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === '/') {
+    fs.readdir('./data', function(err, filelist) {
+      var filteredId = path.parse(queryData.id).base;
+      if (queryData.id === undefined) {
+        filteredId = 'index';
+      }
+      fs.readFile(`./data/${filteredId}`, 'utf-8', function(err, contents) {
+        var html = template.html(contents);
+        response.writeHead(200);
+        response.end(html);
+      });
     });
-  });
-}
-} else {
-  response.writeHead(404);
-  response.end('Not found');
-}
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
 });
-});
-app.listen(5000);
+app.listen(6000);
