5838c93cbff7326a63c4b0d447c755e9870b85bd
EunJin <yeolire@gmail.com>
2018-12-17 17:10:07 +0900

    index--edit scroll

---
+++ script.js
@@ -0,0 +1,32 @@
+scrollPage();
+
+function scrollPage() {
+  var sections = document.querySelectorAll('section');
+
+  window.addEventListener('scroll', move);
+
+  function move() {
+    var current = window.pageYOffset + 1;
+    var height = sections[0].clientHeight;
+    window.addEventListener('resize', function() {
+      height = sections[0].clientHeight;
+    });
+
+    var isChrome = !!window.chrome;
+    if (isChrome) {
+      window.addEventListener('wheel', function(e) {
+        var i = Math.floor(current / height);
+        console.log(i);
+        if (e.deltaY < 0) {
+          sections[i].scrollIntoView({
+            behavior: 'smooth'
+          });
+        } else if (e.deltaY > 0) {
+          sections[i + 1].scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+    }
+  }
+};
